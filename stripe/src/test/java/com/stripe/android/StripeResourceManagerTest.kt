6ff969cfd253aba377691b015c65a4aee4181d8e
Samuel Maskell <smaskell@stripe.com>
2020-05-04 15:28:13 -0700

    Add kotlinx-coroutines-test

--- stripe/src/test/java/com/stripe/android/StripeResourceManagerTest.kt
+++ stripe/src/test/java/com/stripe/android/StripeResourceManagerTest.kt
@@ -1,7 +1,32 @@
 package com.stripe.android
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
+@RunWith(RobolectricTestRunner::class)
 class StripeResourceManagerTest {
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val scope: CoroutineScope = TestCoroutineScope(testDispatcher)
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
 
 }
