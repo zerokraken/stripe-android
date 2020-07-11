d2f4f4ad3ca0c3c79291e61250f8fc64af5577b9
Samuel Maskell <smaskell@stripe.com>
2020-07-10 17:14:51 -0700

    Add bottom sheet activity

---
+++ example/src/main/java/com/stripe/example/activity/BottomSheetActivity.kt
@@ -0,0 +1,11 @@
+package com.stripe.example.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomSheetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_sheet)
+    }
+}
